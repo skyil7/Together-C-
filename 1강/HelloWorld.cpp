0b6a94b63a5fe87af4e6104519cf7e54a2a5261a
skyil7 <giopaik@naver.com>
2019-05-13 14:49:28 +0900

    commit

---
+++ 1강/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>//기본 라이브러리 불러오기
+using namespace std;//std라는 기본 네임스페이스 사용
+
+void main()//메인 함수 선언
+{
+	cout << "Hello World";//Cout == Console OUT의 약자
+	//<< 를 이용해서 화살표 우측의 "Hello World"문자열을 cout으로 전달
+
+	cout << "Hello" << endl;//Hello 뒤에 엔드라인(줄바꿈)을 추가해줌
+	cout << "World";
+
+}
